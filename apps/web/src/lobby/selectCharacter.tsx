dd2098d6479ff3100b3bbaf7966146817905f701
Ahmed El-Sayed <ahmed.elsayed2002@hotmail.com>
2026-06-17 17:21:59 +1000

    if pcs and npcs are empty lists, create a character button exists instead of dropdown for select character

--- apps/web/src/lobby/selectCharacter.tsx
+++ apps/web/src/lobby/selectCharacter.tsx
@@ -6,32 +6,41 @@ type SelectCharacterProps = { roomId: string }
 
 export function SelectCharacter({ roomId }: SelectCharacterProps) {
   const [_character, setCharacter] = useState<string>("")
-  const [pcs, setPcs] = useState<string[]>(["Strahd", "Edward", "Aurelion", "Brumhilde"])
-  const [npcs, setNpcs] = useState<string[]>(["Yousef", "Michael", "Dawnstar", "Lint"]);
+  const [pcs, setPcs] = useState<string[]>([])
+  const [npcs, setNpcs] = useState<string[]>([]);
 
-  return (
-    <Select onValueChange={setCharacter}>
-      <SelectTrigger className="w-full max-w-48">
-        <SelectValue placeholder="Select a Character"/>
-      </SelectTrigger>
-      <SelectContent className="bg-background">
-        <SelectGroup hidden={pcs.length === 0}>
-          <SelectLabel>PCs</SelectLabel>
-          {/* Should be disabled if character has been selected*/}
-          {pcs.map((name) => (
-            <SelectItem disabled={name==="Aurelion"}value={name}>{name}</SelectItem>
-          ))}
-        </SelectGroup>
-        <SelectGroup> {/* Should be hidden if player is not GM*/}
-          <SelectSeparator/>
-          <SelectLabel>NPCs</SelectLabel>
-          {npcs.map((name) => (
-            <SelectItem value={name}>{name}</SelectItem>
-          ))}
-        </SelectGroup>
-        <hr />
+  if (pcs.length > 0 || npcs.length > 0) {
+    return (
+      <Select onValueChange={setCharacter}>
+        <SelectTrigger className="w-full max-w-48">
+          <SelectValue placeholder="Select a Character"/>
+        </SelectTrigger>
+        <SelectContent className="bg-background">
+          <SelectGroup hidden={pcs.length === 0}>
+            <SelectLabel>PCs</SelectLabel>
+            {/* Should be disabled if character has been selected*/}
+            {pcs.map((name) => (
+              <SelectItem disabled={name==="Aurelion"}value={name}>{name}</SelectItem>
+            ))}
+          </SelectGroup>
+          <SelectGroup hidden={npcs.length === 0}> {/* Should be hidden if player is not GM*/}
+            <SelectSeparator/>
+            <SelectLabel>NPCs</SelectLabel>
+            {npcs.map((name) => (
+              <SelectItem value={name}>{name}</SelectItem>
+            ))}
+          </SelectGroup>
+          <hr />
+          <CreateCharacter roomId={roomId} pcs={pcs} setNpcs={setNpcs} setPcs={setPcs} npcs={npcs}/>
+        </SelectContent>
+      </Select>
+    )
+  } else {
+    return (
+      <div className="w-36">
         <CreateCharacter roomId={roomId} pcs={pcs} setNpcs={setNpcs} setPcs={setPcs} npcs={npcs}/>
-      </SelectContent>
-    </Select>
+      </div>
   )
+  }
+
 }
